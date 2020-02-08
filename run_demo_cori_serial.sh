8f0c2c1d7e66da850844abae4c33e55b6d5d922c
Burlen Loring <bloring@lbl.gov>
2020-02-07 16:42:53 -0800

    pass arguments to run scripts

--- run_demo_cori_serial.sh
+++ run_demo_cori_serial.sh
@@ -12,7 +12,7 @@ last=-1
 python3 ./teca_jet_stream_sinuosity \
     --first_step=${first} --last_step=${last} \
     --input_regex=${data_dir}/cam5_1_amip_run2.cam2.h2.1980-12-'.*\.nc$' \
-    --area_threshold 7.e6 --output_file=sinuosity.bin
+    --area_threshold 7.e6 --output_file=sinuosity.bin $*
 
 python3 ./plot_sinuosity.py ./sinuosity.bin ./sinuosity.png
 
